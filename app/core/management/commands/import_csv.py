c58429c6a64b0c0bc5747a84145ec4af44288a51
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-02-12 13:09:33 +0100

    Add django command to import pokemon csv file.

---
+++ app/core/management/commands/import_csv.py
@@ -0,0 +1,64 @@
+import csv
+import os
+
+from core.models import PokedexCreature
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """
+    Create PokedexCreature instances from CSV file
+    """
+
+    help = "Import pokemon CSV file and create PokedexCreature instances"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "csv_file_path",
+            type=str,
+            nargs="?",
+            default=os.path.join(settings.BASE_DIR, "pokemon.csv"),
+        )
+
+    def handle(self, *args, **options):
+        csv_file_path = options.get("csv_file_path", None)
+        if csv_file_path and csv_file_path.endswith(".csv"):
+            with open(csv_file_path, newline="") as csvfile:
+                reader = csv.reader(csvfile)
+                # skip the headers:
+                next(reader, None)
+
+                creatures = [
+                    PokedexCreature(
+                        ref_number=row[0],
+                        name=row[1],
+                        type_1=row[2],
+                        type_2=row[3],
+                        total=int(row[4]),
+                        hp=int(row[5]),
+                        attack=int(row[6]),
+                        defense=int(row[7]),
+                        special_attack=int(row[8]),
+                        special_defence=int(row[9]),
+                        speed=int(row[10]),
+                        generation=int(row[11]),
+                        legendary=bool(row[12]),
+                    )
+                    for row in reader
+                ]
+
+                PokedexCreature.objects.bulk_create(
+                    creatures,
+                    batch_size=100,
+                    ignore_conflicts=True,
+                )
+
+                self.stdout.write(
+                    self.style.SUCCESS(
+                        f"Nb of creatures imported to the database: "
+                        f"{len(creatures)}."
+                    )
+                )
+        else:
+            self.stderr.write(self.style.ERROR("This is not a CSV file."))
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,5 +1,10 @@
+import os
+import tempfile
 from unittest.mock import patch
 
+import pytest
+from core.models import PokedexCreature
+from django.conf import settings
 from django.core.management import call_command
 from django.db.utils import OperationalError
 
@@ -20,3 +25,32 @@ def test_wait_for_db(ts):
         gi.side_effect = [OperationalError] * 5 + [True]
         call_command("wait_for_db")
         assert gi.call_count == 6
+
+
+@pytest.mark.django_db
+def test_import_pokemon_csv(capsys) -> None:
+    """Test import pokemon csv file in app directory"""
+    call_command("import_csv")
+    assert PokedexCreature.objects.count() == 800
+    expected = "Nb of creatures imported to the database: 800.\n"
+    assert expected == capsys.readouterr().out
+
+
+@pytest.mark.django_db
+def test_import_other_csv_file(capsys) -> None:
+    """Test import another CSV file"""
+    call_command(
+        "import_csv", os.path.join(settings.BASE_DIR, "core/tests/test.csv")
+    )
+    assert PokedexCreature.objects.count() == 4
+    expected = "Nb of creatures imported to the database: 4.\n"
+    assert expected == capsys.readouterr().out
+
+
+@pytest.mark.django_db
+def test_import_invalid_csv_file(capsys) -> None:
+    """Test import invalid CSV file"""
+    with tempfile.NamedTemporaryFile(suffix=".jpg") as ntf:
+        call_command("import_csv", ntf.name)
+        expected = "This is not a CSV file.\n"
+        assert expected == capsys.readouterr().err

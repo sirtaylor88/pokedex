be0ff4f6dde3398c2b1bd1e14e81e7fb252465b2
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2022-02-13 16:06:30 +0100

    Add more pokedex creatured filter tests.

--- app/api/filters.py
+++ app/api/filters.py
@@ -4,24 +4,8 @@ from core.models import PokedexCreature
 
 class PokedexCreatureFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr="icontains")
-    TYPE_1_CHOICES = [
-        (choice, choice)
-        for choice in PokedexCreature.objects.all()
-        .order_by("type_1")
-        .values_list("type_1", flat=True)
-        .distinct()
-    ]
-    TYPE_2_CHOICES = [
-        (choice, choice)
-        for choice in PokedexCreature.objects.filter(type_2__isnull=False)
-        .exclude(type_2="")
-        .order_by("type_2")
-        .values_list("type_2", flat=True)
-        .distinct()
-    ]
-
-    type_1 = django_filters.ChoiceFilter(choices=TYPE_1_CHOICES)
-    type_2 = django_filters.ChoiceFilter(choices=TYPE_2_CHOICES)
+    type_1 = django_filters.CharFilter(lookup_expr="iexact")
+    type_2 = django_filters.CharFilter(lookup_expr="iexact")
 
     class Meta:
         model = PokedexCreature
--- app/api/tests/test_pokedex_api.py
+++ app/api/tests/test_pokedex_api.py
@@ -25,7 +25,7 @@ def test_listing_pokedex_creatures(pokedex_creature_factory):
 
     assert res.status_code == status.HTTP_200_OK
     assert len(serializer.data) == 2
-    assert serializer.data == res.data.get("results")
+    assert serializer.data == res.data["results"]
 
 
 def test_view_pokedex_creature_detail(pokedex_creature_factory):
@@ -42,3 +42,115 @@ def test_view_pokedex_creature_detail(pokedex_creature_factory):
 
     assert res.status_code == status.HTTP_200_OK
     assert serializer.data == res.data
+
+
+def test_filter_pokedex_creatures_by_name(pokedex_creature_factory):
+    """Test filtering Pokedex creatures based on their name"""
+
+    creature_1 = pokedex_creature_factory(name="Pikachu")
+    creature_2 = pokedex_creature_factory(name="Goldduck")
+    creature_3 = pokedex_creature_factory(name="Goldeen")
+
+    client = APIClient()
+    res = client.get(
+        reverse("api:pokedex-list"),
+        {"name": "gold"},
+    )
+
+    assert res.status_code == status.HTTP_200_OK
+
+    serializer_1 = PokedexCreatureSerializer(creature_1)
+    serializer_2 = PokedexCreatureSerializer(creature_2)
+    serializer_3 = PokedexCreatureSerializer(creature_3)
+
+    assert serializer_1.data not in res.data["results"]
+    assert serializer_2.data in res.data["results"]
+    assert serializer_3.data in res.data["results"]
+
+
+def test_filter_pokedex_creatures_by_type_one(pokedex_creature_factory):
+    """Test filtering Pokedex creatures based on their type 1"""
+    creature_1 = pokedex_creature_factory(type_1="Poison")
+    creature_2 = pokedex_creature_factory(type_1="Flying")
+    creature_3 = pokedex_creature_factory(type_1="Dragon")
+
+    client = APIClient()
+    res = client.get(
+        reverse("api:pokedex-list"),
+        {"type_1": "Dragon"},
+    )
+    assert res.status_code == status.HTTP_200_OK
+
+    serializer_1 = PokedexCreatureSerializer(creature_1)
+    serializer_2 = PokedexCreatureSerializer(creature_2)
+    serializer_3 = PokedexCreatureSerializer(creature_3)
+
+    assert serializer_1.data not in res.data["results"]
+    assert serializer_2.data not in res.data["results"]
+    assert serializer_3.data in res.data["results"]
+
+
+def test_filter_pokedex_creatures_by_type_two(pokedex_creature_factory):
+    """Test filtering Pokedex creatures based on their type 2"""
+    creature_1 = pokedex_creature_factory(type_2="Poison")
+    creature_2 = pokedex_creature_factory(type_2="Flying")
+    creature_3 = pokedex_creature_factory(type_2=None)
+
+    client = APIClient()
+    res = client.get(
+        reverse("api:pokedex-list"),
+        {"type_2": "Flying"},
+    )
+    assert res.status_code == status.HTTP_200_OK
+
+    serializer_1 = PokedexCreatureSerializer(creature_1)
+    serializer_2 = PokedexCreatureSerializer(creature_2)
+    serializer_3 = PokedexCreatureSerializer(creature_3)
+
+    assert serializer_1.data not in res.data["results"]
+    assert serializer_2.data in res.data["results"]
+    assert serializer_3.data not in res.data["results"]
+
+
+def test_filter_pokedex_creatures_by_generation(pokedex_creature_factory):
+    """Test filtering Pokedex creatures based on their generation"""
+    creature_1 = pokedex_creature_factory(generation=5)
+    creature_2 = pokedex_creature_factory(generation=4)
+    creature_3 = pokedex_creature_factory(generation=5)
+
+    client = APIClient()
+    res = client.get(
+        reverse("api:pokedex-list"),
+        {"generation": 5},
+    )
+    assert res.status_code == status.HTTP_200_OK
+
+    serializer_1 = PokedexCreatureSerializer(creature_1)
+    serializer_2 = PokedexCreatureSerializer(creature_2)
+    serializer_3 = PokedexCreatureSerializer(creature_3)
+
+    assert serializer_1.data in res.data["results"]
+    assert serializer_2.data not in res.data["results"]
+    assert serializer_3.data in res.data["results"]
+
+
+def test_filter_pokedex_creatures_by_legendary(pokedex_creature_factory):
+    """Test filtering Pokedex creatures based on their legendary status"""
+    creature_1 = pokedex_creature_factory(legendary=False)
+    creature_2 = pokedex_creature_factory(legendary=True)
+    creature_3 = pokedex_creature_factory(legendary=False)
+
+    client = APIClient()
+    res = client.get(
+        reverse("api:pokedex-list"),
+        {"legendary": True},
+    )
+    assert res.status_code == status.HTTP_200_OK
+
+    serializer_1 = PokedexCreatureSerializer(creature_1)
+    serializer_2 = PokedexCreatureSerializer(creature_2)
+    serializer_3 = PokedexCreatureSerializer(creature_3)
+
+    assert serializer_1.data not in res.data["results"]
+    assert serializer_2.data in res.data["results"]
+    assert serializer_3.data not in res.data["results"]
